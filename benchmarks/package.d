8e49c74864b8c07c5f3221eadf2244978ffdf371
Carter Anderson <mcanders1@gmail.com>
2018-02-21 22:43:20 -0800

    uncomment and use the new GodotNativeLibrary in package.d

--- benchmarks/package.d
+++ benchmarks/package.d
@@ -11,24 +11,14 @@ public import bunnymark.v1.sprites;
 
 // This is supposed to work with latest godot-d, but it doesn't work with Godot 3.0
 
-// mixin GodotNativeLibrary!
-// (
-// 	"godot", // same as the symbol_prefix in the GDNativeLibrary resource
-
-// 	BunnymarkV2,
-// 	BunnymarkV1DrawTexture,
-// 	BunnymarkV1Sprites,
-
-// 	(GodotInitOptions o){ writeln("Library initialized"); },
-// 	(GodotTerminateOptions o){ writeln("Library terminated"); }
-// );
-
-mixin GodotNativeInit!
+mixin GodotNativeLibrary!
 (
-    "godot_",
-    BunnymarkV2,
-    BunnymarkV1DrawTexture,
-    BunnymarkV1Sprites,
-);
+	"godot_", // same as the symbol_prefix in the GDNativeLibrary resource
 
-mixin GodotNativeTerminate!("godot_");
+	BunnymarkV2,
+	BunnymarkV1DrawTexture,
+	BunnymarkV1Sprites,
+
+	(GodotInitOptions o){ writeln("Library initialized"); },
+	(GodotTerminateOptions o){ writeln("Library terminated"); }
+);
